234ec17232c70485ea68547c51fc1bf63ab5de1b
Dekoder-py <kyle@codingcorner.dev>
2026-02-15 20:08:55 +1300

    set up interactive prompt / runFile + add error reporting

--- dev/codingcorner/jale/Jale.java
+++ dev/codingcorner/jale/Jale.java
@@ -1,9 +1,67 @@
 // Jale, by Kyle. Jale (Java + Kyle) is a scripting language written in Java.
 package dev.codingcorner.jale;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Jale {
-  public static void main(String[] args) {
-    System.out.println("Hello, Jale!");
+  static boolean hadError = false;
+
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      // User entered to many args, return error and show usage.
+      System.out.println("Usage: jale [script]");
+      System.exit(64);
+    } else if (args.length == 1) {
+      // User provided the script as an arg - run it.
+      runFile(args[0]);
+    } else {
+      // User ran with no args, show interactive prompt.
+      runPrompt();
+    }
   }
- 
+
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
+    if (hadError) System.exit(65);
+  }
+
+  private static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    for (;;) {
+      System.out.print("> ");
+      String line = reader.readLine();
+      if (line == null)
+        break; // if there is no input, quit the interactive prompt.
+      run(line);
+      hadError = false; // no need to kill the interactive prompt if there was an error.
+    }
+  }
+
+  private static void run(String source) {
+    Scanner scanner = new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
+
+    for (Token token : tokens) {
+      System.out.println(token);
+    }
+  }
+
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
+
+  private static void report(int line, String where, String message) {
+    System.err.println("[line " + line + "] Error" + where + ": " + message);
+    hadError = true;
+  }
+
 }
