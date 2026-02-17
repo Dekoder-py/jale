2b4b7ba9e849c02e6d4440dab2b4d08f351e151d
Dekoder-py <kyle@codingcorner.dev>
2026-02-18 06:59:49 +1300

    Create Interpreter class and add the literal handling

---
+++ dev/codingcorner/jale/Interpreter.java
@@ -0,0 +1,8 @@
+package dev.codingcorner.jale;
+
+class Interpreter implements Expr.Visitor<Object> {
+  @Override
+  public Object visitLiteralExpr(Expr.Literal expr) {
+    return expr.value;
+  }
+}
