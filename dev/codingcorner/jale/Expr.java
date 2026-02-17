73ad5a89b498281fca287c3befdc10d3dfde5c36
Dekoder-py <kyle@codingcorner.dev>
2026-02-17 19:48:13 +1300

    Re-run GenerateAst to fix Expr.java

--- dev/codingcorner/jale/Expr.java
+++ dev/codingcorner/jale/Expr.java
@@ -18,7 +18,7 @@ abstract class Expr {
 
     @Override
     <R> R accept(Visitor<R> visitor) {
-      return visitor.vistBinaryExpr(this);
+      return visitor.visitBinaryExpr(this);
     }
 
     final Expr left;
@@ -32,7 +32,7 @@ abstract class Expr {
 
     @Override
     <R> R accept(Visitor<R> visitor) {
-      return visitor.vistGroupingExpr(this);
+      return visitor.visitGroupingExpr(this);
     }
 
     final Expr expression;
@@ -44,7 +44,7 @@ abstract class Expr {
 
     @Override
     <R> R accept(Visitor<R> visitor) {
-      return visitor.vistLiteralExpr(this);
+      return visitor.visitLiteralExpr(this);
     }
 
     final Object value;
@@ -57,7 +57,7 @@ abstract class Expr {
 
     @Override
     <R> R accept(Visitor<R> visitor) {
-      return visitor.vistUnaryExpr(this);
+      return visitor.visitUnaryExpr(this);
     }
 
     final Token operator;
