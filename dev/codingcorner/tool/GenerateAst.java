f6f3f96f0ddeda5a1a1cb1e79491607044186074
Dekoder-py <kyle@codingcorner.dev>
2026-02-17 19:09:38 +1300

    add defineVisitor method

--- dev/codingcorner/tool/GenerateAst.java
+++ dev/codingcorner/tool/GenerateAst.java
@@ -13,14 +13,14 @@ public class GenerateAst {
     }
     String outputDir = args[0];
     defineAst(outputDir, "Expr",
-        Arrays.asList("Binary   : Expr left, Token operator, Expr right",
-            "Grouping : Expr expression",
-            "Literal  : Object value",
-            "Unary    : Token operator, Expr right"));
+              Arrays.asList("Binary   : Expr left, Token operator, Expr right",
+                            "Grouping : Expr expression",
+                            "Literal  : Object value",
+                            "Unary    : Token operator, Expr right"));
   }
 
   private static void defineAst(String outputDir, String baseName,
-      List<String> types) throws IOException {
+                                List<String> types) throws IOException {
     String path = outputDir + "/" + baseName + ".java";
     PrintWriter writer = new PrintWriter(path, "UTF-8");
 
@@ -29,6 +29,8 @@ public class GenerateAst {
     writer.println("import java.util.List;");
     writer.println();
     writer.println("abstract class " + baseName + " {");
+
+    defineVisitor(writer, baseName, types);
 
     // Write the AST classes
     for (String type : types) {
@@ -41,10 +43,23 @@ public class GenerateAst {
     writer.close();
   }
 
+  private static void defineVisitor(PrintWriter writer, String baseName,
+                                    List<String> types) {
+    writer.println("  interface Visitor<R> {");
+
+    for (String type : types) {
+      String typeName = type.split(":")[0].trim();
+      writer.println("    R visit" + typeName + baseName + "(" + typeName +
+                     " " + baseName.toLowerCase() + ");");
+    }
+
+    writer.println("  }");
+  }
+
   private static void defineType(PrintWriter writer, String baseName,
-      String className, String fieldList) {
+                                 String className, String fieldList) {
     writer.println(" static class " + className + " extends " + baseName +
-        " {");
+                   " {");
 
     // Constructor
     writer.println("    " + className + "(" + fieldList + ") {");
